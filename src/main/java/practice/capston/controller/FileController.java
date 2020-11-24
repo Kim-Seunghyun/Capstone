3bd0e6b6810adb9732379cacfc1d448935fc9330
ygSonggoon <brutowa13@gmail.com>
2020-11-24 23:28:05 +0900

    [fix] view에 맞추어서 컨트롤단을 수정한 내용입니다.
    사전에 언급한 대로 기본적인 기능을 할 수 있도록 만들었습니다.
    다음주 중에 리팩토링을 할 예정입니다. 추후에 자세히 작성하도록 하겠습니다.

--- src/main/java/practice/capston/controller/FileController.java
+++ src/main/java/practice/capston/controller/FileController.java
@@ -1,7 +1,72 @@
 package practice.capston.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import practice.capston.domain.entity.Image;
+import practice.capston.domain.entity.Member;
+import practice.capston.domain.entity.TextContent;
+import practice.capston.repository.TextContentRepository;
+import practice.capston.service.ImageService;
+import practice.capston.service.TextContentService;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
+@RequiredArgsConstructor
 public class FileController {
+
+    private final ImageService imageService;
+    private final TextContentService textContentService;
+
+    @GetMapping("/summaryService")
+    public String summaryService(@AuthenticationPrincipal Member member) {
+        return "summary/fileupload";
+    }
+
+    @PostMapping("/summaryService")
+    public String fileUpload(@RequestParam("file") MultipartFile multipartFile,
+                             @AuthenticationPrincipal Member member) throws IOException {
+
+
+        Image saveImage = imageService.saveImage(member.getUsername() + multipartFile.getOriginalFilename(), member);
+        String path = saveImage.getPath();
+
+        // 시언어 짜듯이 작성을 하게 되었는데...  리팩토링이 필요한 부분.
+        // 근데 생각을 해보면, 버퍼에다가 데이터를 입력하는 부분이므로 필요한 변수가 꽤 많을 수 밖에 없고,
+        // 파일 클래스를 별도로 만들어서 작업하는것은 오히려 별로라고 판단되는게, 컨트롤단의 역할로서 적당하다고 생각되기 때문에 고민 중.
+        try{
+            int bufferSize = 1024;
+            int len = 0;
+            int minusOne = -1;
+            byte[] buffer = new byte[bufferSize];
+
+            //쓰기 전용 스트림
+            FileOutputStream fileOutputStream = new FileOutputStream(path + member.getUsername() + multipartFile.getOriginalFilename());
+
+            //읽기 전용 스트림
+            InputStream inputStream = multipartFile.getInputStream();
+
+            while((len = inputStream.read(buffer)) != minusOne){
+                fileOutputStream.write(buffer, 0, len);
+            }
+
+        }catch (Exception exception) {
+            imageService.deleteImage(saveImage, member);
+            throw new FileNotFoundException("FileController: 59");
+        } finally {
+            // 카프카 메세지에 이미지 DTO를 던지는 부분.
+            textContentService.createTextContent("result test", saveImage.getId());
+        }
+
+
+        return "redirect:/";
+    }
 }
--- src/main/java/practice/capston/controller/HomeController.java
+++ src/main/java/practice/capston/controller/HomeController.java
@@ -1,34 +1,66 @@
 package practice.capston.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import practice.capston.domain.dto.MemberLoginDto;
+import practice.capston.domain.entity.Member;
+import practice.capston.service.MemberService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/")
     public String home() {
-        return "/home/home";
+        return "home/home";
     }
 
     @GetMapping("/register")
     public String register() {
-        return "/home/register";
+        return "home/register";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "/home/login";
-    }
+    @PostMapping("/register")
+    public String register(@ModelAttribute("registerForm") @Validated MemberLoginDto memberLoginDto) {
 
-    @GetMapping("/logout")
-    public String logout() {
+        memberLoginDto.setPassword(passwordEncoder.encode(memberLoginDto.getPassword()));
+        Member member = new Member(memberLoginDto);
+        memberService.joinMember(member);
         return "redirect:/";
     }
 
-    @PostMapping("/login_proc")
-    public String loginProc() {
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required=false) String error,
+                        @RequestParam(value = "exception", required = false) String exception,
+                        Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+        return "home/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
         return "redirect:/";
     }
 }
